dbdea2c1218378e9db382d5e7fdb586336018062
rachelkd <rachel05deng@gmail.com>
2024-03-23 22:30:32 -0400

    first commit

---
+++ load_data.py
@@ -0,0 +1,20 @@
+"""CSC111 Winter 2024 Project 2:
+How Do We Create Playlists? An Investigation into Song Recommendations Based on Similarity and Occurrence in Playlists
+
+Module Description
+==================
+This module loads the data and creates the graph needed for our project.
+
+Copyright and Usage Information
+===============================
+This file is Copyright (c) 2024 Rachel Deng, Ben Henderson, Jeha Park
+"""
+
+
+if __name__ == '__main__':
+    import python_ta
+    python_ta.check_all(config={
+        'extra-imports': [],  # the names (strs) of imported modules
+        'allowed-io': ['print', 'open', 'input'],  # the names (strs) of functions that call print/open/input
+        'max-line-length': 120
+    })
---
+++ main.py
@@ -0,0 +1,11 @@
+"""CSC111 Winter 2024 Project 2:
+How Do We Create Playlists? An Investigation into Song Recommendations Based on Similarity and Occurrence in Playlists
+
+Module Description
+==================
+This module runs the playlist recommendation algorithm and creates visualizations as such.
+
+Copyright and Usage Information
+===============================
+This file is Copyright (c) 2024 Rachel Deng, Ben Henderson, Jeha Park
+"""
