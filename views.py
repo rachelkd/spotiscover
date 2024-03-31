ce99a9f8b56de4f1eb5e56c920068f10e8a50794
rachelkd <rachel05deng@gmail.com>
2024-03-31 15:41:45 -0400

    text changes

--- views.py
+++ views.py
@@ -1,3 +1,14 @@
+"""CSC111 Winter 2024 Project 2:
+How Do We Create Playlists? An Investigation into Song Recommendations Based on Similarity and Occurrence in Playlists
+
+Module Description
+==================
+This module manages the directories of the web application.
+
+Copyright and Usage Information
+===============================
+This file is Copyright (c) 2024 Rachel Deng, Ben Henderson, Jeha Park
+"""
 from flask import Blueprint, render_template
 
 views = Blueprint('views', __name__)
