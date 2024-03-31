14ce42ff0c4cf5b4119ab07f455cfbc6f91fe6a0
rachelkd <rachel05deng@gmail.com>
2024-03-31 15:23:30 -0400

    added flask to requirements

--- views.py
+++ views.py
@@ -2,9 +2,11 @@ from flask import Blueprint, render_template
 
 views = Blueprint('views', __name__)
 
+
 @views.route('/')
 def home():
     return render_template("base.html")
+
 
 @views.route('/recomendation')
 def rec():
