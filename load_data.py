11c9d93375b697956f9a9da3aaab6268c46e3df4
rachelkd <rachel05deng@gmail.com>
2024-03-31 15:08:57 -0400

    updated pyTA allowed IO

--- load_data.py
+++ load_data.py
@@ -82,6 +82,6 @@ if __name__ == '__main__':
     import python_ta
     python_ta.check_all(config={
         'extra-imports': ['json', 'classes'],  # the names (strs) of imported modules
-        'allowed-io': ['print', 'open', 'input'],  # the names (strs) of functions that call print/open/input
+        'allowed-io': ['load_graph', 'print', 'input'],  # the names (strs) of functions that call print/open/input
         'max-line-length': 120
     })
