54ce2b0b0c4b7262f872bb073d9cd436c458ccce
rachelkd <rachel05deng@gmail.com>
2024-04-03 01:14:13 -0400

    fixed pyTA errors

--- views.py
+++ views.py
@@ -18,24 +18,24 @@ PLAYLIST_GRAPH, TRACKS_TO_OBJECTS, URIS_TO_OBJECTS = load_graph(['data/mpd.slice
                                                                  'data/mpd.slice.1000-1999.json',
                                                                  'data/mpd.slice.2000-2999.json'])
 
-views = Blueprint('views', __name__)
-liked_songs_so_far = set()
+VIEWS = Blueprint('views', __name__)
+LIKED_SONGS_SO_FAR = set()
 
 
-@views.route('/', methods=['GET', 'POST'])
-@views.route('/')
-def home():
-    global liked_songs_so_far
+@VIEWS.route('/', methods=['GET', 'POST'])
+@VIEWS.route('/')
+def home() -> str:
+    global LIKED_SONGS_SO_FAR
 
     if request.method == 'POST':
         response = request.form.get('rdo')  # Get the value of the radio button
 
         if response == 'yes':
-            liked_songs_so_far.add(session['track'])
+            LIKED_SONGS_SO_FAR.add(session['track'])
 
-    if len(liked_songs_so_far) >= 3:
-        recs = PLAYLIST_GRAPH.get_recommendations(tracks_liked=liked_songs_so_far)
-        liked_songs_so_far.clear()  # Clear liked_songs_so_far after generating recommendations
+    if len(LIKED_SONGS_SO_FAR) >= 3:
+        recs = PLAYLIST_GRAPH.get_recommendations(tracks_liked=LIKED_SONGS_SO_FAR)
+        LIKED_SONGS_SO_FAR.clear()  # Clear liked_songs_so_far after generating recommendations
 
         uris = [rec[0].track_uri[14:] for rec in recs]
         return render_template('rec.html', uris=uris)
@@ -45,6 +45,16 @@ def home():
         uri = track.track_uri[14:]
         return render_template("base.html", uri=uri)
 
-@views.route('/recomendation')
-def rec():
+
+@VIEWS.route('/recomendation')
+def rec() -> str:
     return render_template('rec.html')
+
+
+if __name__ == '__main__':
+    import python_ta
+    python_ta.check_all(config={
+        'extra-imports': ['flask', 'input', 'load_data'],  # the names (strs) of imported modules
+        'allowed-io': ['print', 'open', 'input'],  # the names (strs) of functions that call print/open/input
+        'max-line-length': 120
+    })
