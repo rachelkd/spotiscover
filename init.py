721fd977c7b29fbc50c91e11961b1067a8c0a6ad
benrockehenderson <benrockehenderson@gmail.com>
2024-04-03 00:54:12 -0400

    Starting a session

--- init.py
+++ init.py
@@ -2,6 +2,7 @@ from flask import Flask
 
 def create_app():
     app = Flask(__name__)
+    app.secret_key = "this_is_not_important_key:)"
     from views import views
 
     app.register_blueprint(views, url_prefix='/')
--- views.py
+++ views.py
@@ -1,4 +1,16 @@
-from flask import Blueprint, render_template, request
+"""CSC111 Winter 2024 Project 2:
+How Do We Create Playlists? An Investigation into Song Recommendations Based on Similarity and Occurrence in Playlists
+
+Module Description
+==================
+This module manages the directories of the web application.
+
+Copyright and Usage Information
+===============================
+This file is Copyright (c) 2024 Rachel Deng, Ben Henderson, Jeha Park
+"""
+
+from flask import Blueprint, render_template, request, session
 from input import return_random_track
 from load_data import load_graph
 from track_to_spotify_track import create_track_from_api_response, api_response, get_track_embed_html
@@ -8,46 +20,30 @@ PLAYLIST_GRAPH, TRACKS_TO_OBJECTS = load_graph(['data/mpd.slice.0-999.json',
                                                 'data/mpd.slice.2000-2999.json'])
 
 views = Blueprint('views', __name__)
-liked_songs_so_far = set() 
+liked_songs_so_far = set()
 
 @views.route('/', methods=['GET', 'POST'])
+@views.route('/')
 def home():
-    global liked_songs_so_far  # Declare liked_songs_so_far as global
-    global liked_songs_so_far_set
-    track = return_random_track(PLAYLIST_GRAPH, [])
-    uri = track.track_uri[14:]
-    uris = set()
+    global liked_songs_so_far
+    
     if request.method == 'POST':
-        response = request.form['rdo']
+        response = request.form.get('rdo')  # Get the value of the radio button
+        
         if response == 'yes':
-            liked_songs_so_far.add(track)  
+            liked_songs_so_far.add(session['track'])
+    
     if len(liked_songs_so_far) >= 3:
         recs = PLAYLIST_GRAPH.get_recommendations(tracks_liked=liked_songs_so_far)
-        liked_songs_so_far = set()
+        liked_songs_so_far.clear()  # Clear liked_songs_so_far after generating recommendations
         
-        for rec in recs:
-            u = rec[0].track_uri[14:]
-            uris.add(u)
+        uris = [rec[0].track_uri[14:] for rec in recs]
         return render_template('rec.html', uris=uris)
-
-    else: 
-        # track = return_random_track(PLAYLIST_GRAPH, [])
-        # uri = track.track_uri[14:]
-        # track_html = get_track_embed_html(uri)
+    else:
+        track = return_random_track(PLAYLIST_GRAPH, [])
+        session['track'] = track
+        uri = track.track_uri[14:]
         return render_template("base.html", uri=uri)
-
-    # else: # GET METHOD
-    #     track = return_random_track(PLAYLIST_GRAPH, [])
-    #     uri = track.track_uri[14:]
-    #     track_html = get_track_embed_html(uri)
-    
-    #     return render_template("base.html", uri=uri)
-        
-
-
-
-    
-
 @views.route('/recomendation')
 def rec():
     return render_template('rec.html')
