c441614927360ad44daa77bd490735b6ade9dbea
rachelkd <rachel05deng@gmail.com>
2024-04-03 22:34:40 -0400

    rename constants

--- main.py
+++ main.py
@@ -9,24 +9,17 @@ Copyright and Usage Information
 ===============================
 This file is Copyright (c) 2024 Rachel Deng, Ben Henderson, Jeha Park
 """
-from load_data import load_graph
 from init import create_app
 
-# PLAYLIST_GRAPH, TRACKS_TO_OBJECTS = load_graph(['data/mpd.slice.0-999.json',
-#                                                 'data/mpd.slice.1000-1999.json',
-#                                                 'data/mpd.slice.2000-2999.json'])
 
-# manage_io(PLAYLIST_GRAPH)  TODO: DELETE UPON SUBMISSION
-# visualize_graph(PLAYLIST_GRAPH, max_vertices=800)  TODO: DELETE UPON SUBMISSION
-
-app = create_app()
+APP = create_app()
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    APP.run(debug=True)
     # import python_ta
     # python_ta.check_all(config={
-    #     'extra-imports': ['json', 'classes'],  # the names (strs) of imported modules
+    #     'extra-imports': ['init'],  # the names (strs) of imported modules
     #     'allowed-io': ['print', 'open', 'input'],  # the names (strs) of functions that call print/open/input
     #     'max-line-length': 120
     # })
